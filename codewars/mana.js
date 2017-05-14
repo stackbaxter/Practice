57a8cc9bf371ebf0d107f2a203a0857f35ae00c7
stackbaxter <stackbaxter@gmail.com>
2017-05-13 21:27:25 -0500

    closer on the mana

--- codewars/mana.js
+++ codewars/mana.js
@@ -6,95 +6,145 @@ let cost;
 let pointsLeft;
 let clearCost;
 let coloredCost;
-let ccost;
+let spentSpells = [];
+let cCost;
+//spell eval function, ideally just pass spell, option and cost
+//
+function speleval(spell, cost){
 
-//think about split method
-function canCast(mana, cost, option) {
-  if(mana === cost){
-    return true;
-  }
-//evaluating the mana
-  if(!isNaN(mana.charAt())){
-    clearMana = (mana.slice(0,1));
-    coloredMana = mana.slice(1).split('');
-    console.log(clearMana);
-    console.log(coloredMana);
-    if(!isNaN(mana.charAt(1))){
-      clearMana = mana.slice(0,2);
-      coloredMana = mana.slice(3).split('');
-      console.log(coloredMana);
-      console.log(clearMana);
-    }
-
-    }
-  else{coloredMana = mana.split('');}
-    console.log(coloredMana);
+  mana = spell.split('');
+  coloredMana = mana.filter(function(spell){
+    return isNaN(spell);
+    });
+  clearMana = mana.filter(function (spell){
+    return !isNaN(spell);
+    });
+  clearMana = parseInt(clearMana.join(''));
+    console.log('clear mana: '+clearMana);
+    console.log('colored mana: '+coloredMana);
 
 // evaluating the cost
-  if(!isNaN(cost.charAt())){
-      clearCost = (cost.slice(0,1));
-      coloredCost = cost.slice(1).split('');
-      console.log(clearCost);
-      console.log(coloredCost);
-      if(!isNaN(cost.charAt(1))){
-        clearCost = cost.slice(0,2);
-        coloredCost = cost.slice(3).split('');
-        console.log(coloredCost);
-        console.log(clearCost);
-      }
+  cost = cost.split('');
+  clearCost = cost.filter(function(spell){
+    return !isNaN(spell);
+    });
 
-   }
-  else{
-    console.log(cost);
-    console.log(coloredCost = cost.split(''));
-    console.log(coloredCost[0]);
+  clearCost = parseInt(clearCost.join(''));
+  console.log(clearCost);
+
+  coloredCost = cost.filter(function(spell){
+    return isNaN(spell);
+    });
+
+  console.log(coloredCost);
+  cCost = cost.length;
+  for(let i = 0; i < cost.length; i ++){
+    console.log(coloredMana);
+    console.log(coloredMana.indexOf(coloredCost[i]));
+
+    if(coloredMana.includes(coloredCost[i]) === false){
+      console.log(coloredMana.indexOf(coloredCost[i])+'blah');
+         return false;
+      }else{
+
+         //slice the spell off the stash NEED to add check of clearcost and clear mana before declaring win
+        spentSpells.push(coloredMana.splice(coloredMana.indexOf(coloredCost[i]),1));
+        cCost --;
+      }
+    if ((cCost === 0 && spell.length === 0)&& clearMana >= clearCost){
+        return true;
+    }else if(coloredMana.length + clearMana >= clearCost){
+      console.log("clearmana eval");
+
+      return true;
+    }else{return false;}
+
+}
+}
+
+function canCast(mana, cost, option) {
+  //evaluating the mana
+    if(mana === cost){return true;}
+
+    mana = mana.split('');
+    coloredMana = mana.filter(spell => isNaN(spell));
+    clearMana = mana.filter(spell => !isNaN(spell));
+    clearMana = parseInt(clearMana.join(''));
+      console.log('clear mana: '+clearMana);
+      console.log('colored mana: '+coloredMana);
+
+  //If option, evaluate the option
+  if(option){
+  speleval(option, cost)
+
+      cCost = coloredOPtion.length;
+      option = option.split('');
+      let clearOption = option.filter(spell => !isNaN(spell));
+      console.log(clearOption);
+      let coloredOPtion = option.filter(spell => isNaN(spell));
+      console.log(coloredOPtion);
+      for(let i = 0; i <= coloredOPtion.length; i ++){
+        console.log(coloredOPtion);
+          if(coloredOption.includes(coloredCost[i]) === false){
+            console.log(coloredMana.indexOf(coloredCost[i])+'blah');
+               return false;
+            }
+             //slice the spell off the stash NEED to add check of clearcost and clear mana before declaring win
+        spentSpells.push(coloredMana.splice(coloredMana.indexOf(coloredCost[i]),1));
+        cCost --;
+
+      if (cCost === 0){
+        if((coloredMana.length + clearMana) >= clearCost){
+          return true;
+        };
+      };
+  }
 
   }
-   ccost = coloredCost.length;
-   console.log(ccost);
-  // (pointsLeft = coloredMana.length);
 
 
+
+// evaluating the cost
+  cost = cost.split('');
+  clearCost = cost.filter(function(spell){
+    return !isNaN(spell);
+  });
+  clearCost = parseInt(clearCost.join(''))
+  coloredCost = cost.filter(function(spell){
+    return isNaN(spell);
+  });
+
+
+    let cCost = coloredCost.length;
+    // (pointsLeft = coloredMana.length);
+
+    //comparing the cost and the mana first, to save leftover to add to clear
     for(let i = 0; i <= coloredCost.length; i ++){
-     console.log(coloredMana);
-     console.log(coloredMana.indexOf(coloredCost[i]));
+    console.log(coloredMana);
+    console.log(coloredMana.indexOf(coloredCost[i]));
 
-      if(!coloredMana.includes(coloredCost[i])){
-       console.log(coloredMana.indexOf(coloredCost[i])+'blah');
-       console.log(coloredMana, coloredCost);
+    if(coloredMana.includes(coloredCost[i]) === false){
+      console.log(coloredMana.indexOf(coloredCost[i])+'blah');
+         return false;
       }
 
-      else{
-        ccost --;
-        (coloredMana.splice(coloredMana.indexOf(coloredCost[i]), 1));
-        // coloredCost.splice(0, 1);
-        // console.log(coloredCost+'Cost');
-        console.log(coloredMana,'Mana');
+         //slice the spell off the stash NEED to add check of clearcost and clear mana before declaring win
+    spentSpells.push(coloredMana.splice(coloredMana.indexOf(coloredCost[i]),1));
+    cCost --;
 
+    if (cCost === 0){
+      if((coloredMana.length + clearMana) >= clearCost){
+        return true;
+      };
+    };
+}}
 
-      }
-
-      // console.log(coloredMana.length);
-    }
-    console.log(coloredMana.length);
-         if(coloredMana.length > 0){
-          clearMana = coloredMana.length + parseInt(clearMana);
-          console.log(clearMana);
-            if(clearMana >= clearCost){
-          console.log(true);
-            }
-            else{
-              // console.log(coloredMana, coloredCost);
-              console.log(false);
-            }
-
-           }
 
 
   // let coloredMana = mana.slice(1);
   // console.log(coloredMana);
 
-}
+
 
 
 canCast('W', 'W');
